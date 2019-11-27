340391349cbb4ce73b37b157b5adb5b30a8f4c57
Katherine Fritsch <katherinefritsch@KatherisMacBook.lan>
2019-11-26 21:23:05 -0800

    basic functionality

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Spin up the server// Callback to debug
+const port = 8000;
+const server = app.listen(port, function () {
+    console.log(`running on localhost: ${port}`);
+});
 
-// Setup Server
+// Callback function to complete GET '/all'
+app.get('/all', function (req, res) {
+    res.send(projectData);
+});
+
+// Post route
+app.post('/addData', function (req, res) {
+    projectData.push(req.body);
+    res.send('Success!');
+})
+
+// Alternative POST route
+/* app.post('/add', function (req, res) {
+    newEntry = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userResponse: request.body.userResponse
+    };
+
+    projectData.unshift(newEntry);
+}); */
+
+
+  
+  
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,72 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+let appID = "8a0fd8eb79fa61066c0c54afcae314a1";
+let baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    document.getElementById("generate").addEventListener("click", performAction);
+    updateUI('/all')
+});
+
+
+/* Function called by event listener */
+function performAction(event) {
+  console.log("generate");
+  const zipCode = document.getElementById("zip").value;
+  const userResponse = document.getElementById("feelings").value;
+  getWeatherData(baseURL, zipCode, appID).then(function(data) {
+    date_str = new Date(data.dt*1000).toDateString();
+    postData("/addData", {
+      temperature: data.main.temp,
+      date: date_str,
+      userResponse: userResponse
+    });
+    updateUI("/all");
+  });
+}
+
+/* Function to GET Web API Data*/
+const getWeatherData = async (baseURL, zipCode, appID) => {
+  const res = await fetch(baseURL + zipCode + "&appID=" + appID);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+/* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    const newData = await response.text();//.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+/* Function to GET Project Data */
+const updateUI = async (url = "") => {
+  const request = await fetch(url);
+  try {
+    const allData = await request.json();
+    lastdata = allData.pop();
+    document.getElementById("date").innerHTML = lastdata.date;
+    document.getElementById("temp").innerHTML = lastdata.temperature;
+    document.getElementById("content").innerHTML = lastdata.userResponse;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
