53ca709984695e0bad382cadb4d6d32adf740a8b
Katherine Fritsch <katherinefritsch@KatherisMacBook.lan>
2019-12-08 13:27:50 -0800

    Changed Index files

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -5,9 +5,8 @@ const cors = require('cors');
 const dotenv = require("dotenv");
 
 dotenv.config();
+
 console.log(`Your API key is ${process.env.API_KEY}`);
-
-
 // set aylien API credentias //
 var textapi = new aylien({
   application_id: process.env.API_ID,
@@ -23,11 +22,11 @@ textapi.sentiment({
 });
 
 var path = require("path");
-
+const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
 
 const app = express();
-app.use(bodyParser.json())
+
 app.use(express.static("dist"));
 
 console.log(__dirname);
@@ -37,15 +36,14 @@ app.get("/", function(req, res) {
 });
 
 app.get("/api", (req, res) => {
-  textapi.sentiment({ 'text': "I like potatoes" }, (error, response) => {
+  textapi.sentiment({ 'text': "xyxyxy" }, (error, response) => {
     res.send(response);
   });
 });
 
 app.post("/api",(req,res)=>{
-  textapi.sentiment(req.body.text,(error,response)=>{
-    console.log(response);
-    res.send(JSON.stringify(response));
+  textapi.sentiment(req.body,(error,response)=>{
+    res.send(response);
   })
 });
 
@@ -57,18 +55,3 @@ app.listen(8080, function() {
 app.get("/test", function(req, res) {
   res.send(mockAPIResponse);
 });
-
-app.post('/sentiment', (req, res, next) => {
-  if(req.body.text !== " ") {
-    const text = req.body.text;
-    textapi.sentiment({
-      'text': text
-    }, function (error, response) {
-      if (error === null) {
-        res.status(201).send(response);
-      }
-    });
-  } else {
-    res.status(400).json('Bad Request');
-  }
-})
